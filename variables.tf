14cfcdf6fe77f7b5615af5765bb98aab8cf1eff8
wt <a@b.com>
2026-09-16 09:15:42 -0700

    initial commit - removed secrets

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+}# Variable declarations
